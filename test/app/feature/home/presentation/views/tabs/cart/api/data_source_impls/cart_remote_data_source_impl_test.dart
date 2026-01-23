f6b318d422c12f17d251d1df0037954fa5bfb41b
momenhaitham <momenhaithamcod12@gmail.com>
2026-01-23 13:16:44 +0200

    feat(tests): added unit tests for cart remote data source and repository implementations feat: added githup action to check unit testing

---
+++ test/app/feature/home/presentation/views/tabs/cart/api/data_source_impls/cart_remote_data_source_impl_test.dart
@@ -0,0 +1,144 @@
+import 'package:flower_app/app/config/base_response/base_response.dart';
+import 'package:flower_app/app/feature/home/presentation/views/tabs/cart/api/data_source_impls/cart_remote_data_source_impl.dart';
+import 'package:flower_app/app/feature/home/presentation/views/tabs/cart/data/models/cart_dto.dart';
+import 'package:flower_app/app/feature/home/presentation/views/tabs/cart/data/models/clear_user_cart_dto.dart';
+import 'package:flower_app/app/feature/home/presentation/views/tabs/cart/data/models/update_cart_response.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'cart_remote_data_source_impl_test.mocks.dart';
+
+@GenerateMocks([CartRemoteDataSourceImpl])
+void main() {
+  late CartRemoteDataSourceImpl cartRemoteDataSourceImpl;
+  setUpAll(() {
+    cartRemoteDataSourceImpl = MockCartRemoteDataSourceImpl();
+    provideDummy<BaseResponse<UpdateCartResponse>>(SuccessResponse<UpdateCartResponse>(data: UpdateCartResponse()));
+    provideDummy<BaseResponse<ClearUserCartDto>>(SuccessResponse<ClearUserCartDto>(data: ClearUserCartDto()));
+
+  },);
+  
+
+  group("Testing add product to cart function",(){
+  test("Testing add product to cart function with success response", ()async {
+    when(cartRemoteDataSourceImpl.addProductToCart(productId: "dwqe21ewqdssqw",quantity: 2)).thenAnswer (
+      (_)async{
+        return SuccessResponse<UpdateCartResponse>(data: UpdateCartResponse(cart: CartDto(),message: "success",numOfCartItems: 2) );
+      }
+    );
+
+    var response =await cartRemoteDataSourceImpl.addProductToCart(productId: "dwqe21ewqdssqw",quantity: 2);
+    expect(response, isA<SuccessResponse<UpdateCartResponse>>());
+    expect((response as SuccessResponse<UpdateCartResponse>).data.message, equals("success"));
+    expect((response ).data.numOfCartItems, equals(2));
+  },);
+
+  test("Testing add product to cart function with error response", ()async {
+    when(cartRemoteDataSourceImpl.addProductToCart(productId: "dwqe21ewqdssqw",quantity: 2)).thenAnswer (
+      (_)async{
+        return ErrorResponse<UpdateCartResponse>(error: Exception("Failed to add product to cart") );
+      }
+    );
+
+    var response =await cartRemoteDataSourceImpl.addProductToCart(productId: "dwqe21ewqdssqw",quantity: 2);
+    expect(response, isA<ErrorResponse<UpdateCartResponse>>());
+    expect((response as ErrorResponse<UpdateCartResponse>).error, isA<Exception>());
+  },);
+
+  },
+  );
+
+
+  group("Testing get user cart function",(){
+    test("Testing get user cart function with success response", ()async {
+    when(cartRemoteDataSourceImpl.getUserCart()).thenAnswer (
+      (_)async{
+        return SuccessResponse<UpdateCartResponse>(data: UpdateCartResponse(cart: CartDto(),message: "success",numOfCartItems: 2) );
+      }
+    );
+
+    var response =await cartRemoteDataSourceImpl.getUserCart();
+    expect(response, isA<SuccessResponse<UpdateCartResponse>>());
+    expect((response as SuccessResponse<UpdateCartResponse>).data.message, equals("success"));
+
+  },);
+
+  test("Testing get user cart function with error response", ()async {
+    when(cartRemoteDataSourceImpl.getUserCart()).thenAnswer (
+      (_)async{
+        return ErrorResponse<UpdateCartResponse>(error: Exception("Failed to get user cart") );
+      }
+    );
+
+    var response =await cartRemoteDataSourceImpl.getUserCart();
+    expect(response, isA<ErrorResponse<UpdateCartResponse>>());
+    expect((response as ErrorResponse<UpdateCartResponse>).error, isA<Exception>());
+  },);
+
+  }
+  );
+
+  group("Testing remove Specific Item from cart function", () {
+    test("Testing remove product from cart function with success response", ()async {
+
+
+
+
+
+      when(cartRemoteDataSourceImpl.removeSpecificItemFromCart(productId: "dwqe21ewqdssqw")).thenAnswer ((_)async{
+          return SuccessResponse<UpdateCartResponse>(data: UpdateCartResponse(cart: CartDto(),message: "success",numOfCartItems: 2) );
+        }
+      );
+
+      var response =await cartRemoteDataSourceImpl.removeSpecificItemFromCart(productId: "dwqe21ewqdssqw");
+      expect(response, isA<SuccessResponse<UpdateCartResponse>>());
+      expect((response as SuccessResponse<UpdateCartResponse>).data.message, equals("success"));
+    },);
+
+    test("Testing remove product from cart function with error response", () async{
+      when(cartRemoteDataSourceImpl.removeSpecificItemFromCart(productId: "dwqe21ewqdssqw")).thenAnswer (
+        (_)async{
+          return ErrorResponse<UpdateCartResponse>(error: Exception("Failed to remove product from cart") );
+        }
+      );
+
+      var response = await cartRemoteDataSourceImpl.removeSpecificItemFromCart(productId: "dwqe21ewqdssqw");
+      expect(response, isA<ErrorResponse<UpdateCartResponse>>());
+      expect((response as ErrorResponse<UpdateCartResponse>).error, isA<Exception>());
+    },);
+
+  },
+  );
+
+ group("Testing clear cart function", () {
+    test("Testing clear cart function with success response", ()async {
+      when(cartRemoteDataSourceImpl.clearUserCart()).thenAnswer (
+        (_)async{
+          return  SuccessResponse<ClearUserCartDto>(data: ClearUserCartDto(message: "success") );
+        }
+      );
+
+      var response = await cartRemoteDataSourceImpl.clearUserCart();
+      
+      expect(response, isA<SuccessResponse<ClearUserCartDto>>());
+      expect((response as SuccessResponse<ClearUserCartDto>).data.message, equals("success"));
+    },);
+
+    test("Testing clear cart function with error response", ()async {
+      when(cartRemoteDataSourceImpl.clearUserCart()).thenAnswer (
+        (_)async{
+          return ErrorResponse<ClearUserCartDto>(error: Exception("Failed to clear cart") );
+        }
+      );
+
+      var response = await cartRemoteDataSourceImpl.clearUserCart();
+      expect(response, isA<ErrorResponse<ClearUserCartDto>>());
+      expect((response as ErrorResponse<ClearUserCartDto>).error, isA<Exception>());
+    },);
+
+  },
+);
+
+
+}
