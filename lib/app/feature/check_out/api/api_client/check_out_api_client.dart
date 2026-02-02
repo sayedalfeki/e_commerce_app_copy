87f5f366071ac20cefd868adfbbe1af8714b8777
momenhaitham <momenhaithamcod12@gmail.com>
2026-02-02 21:32:31 +0200

    feat(checkout): implement cash on delivery and credit card checkout methods with corresponding DTOs and use cases

--- lib/app/feature/check_out/api/api_client/check_out_api_client.dart
+++ lib/app/feature/check_out/api/api_client/check_out_api_client.dart
@@ -1,9 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flower_app/app/core/endpoint/app_endpoint.dart';
+import 'package:flower_app/app/feature/check_out/data/models/cash_on_delivery_dto.dart';
+import 'package:flower_app/app/feature/check_out/data/models/credit_card_dto.dart';
 import 'package:flower_app/app/feature/check_out/data/models/user_address_response.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 part 'check_out_api_client.g.dart';
+
+
 @injectable
 @RestApi()
 abstract class CheckOutApiClient {
@@ -12,4 +16,10 @@ abstract class CheckOutApiClient {
 
   @GET(AppEndPoint.addresses)
   Future<UserAddressResponse> getUserAddresses();
+
+  @POST(AppEndPoint.cashOnDelivery)
+  Future<CashOnDeliveryDto> checkOutWithCashOnDelivery(@Body() Map<String,dynamic> body);
+
+  @POST(AppEndPoint.creditCard)
+  Future<CreditCardDto> checkOutWithCreditCard(@Body() Map<String,dynamic> body);
 }
--- lib/app/feature/check_out/data/data_sources/remote/check_out_remote_data_source_contract.dart
+++ lib/app/feature/check_out/data/data_sources/remote/check_out_remote_data_source_contract.dart
@@ -1,6 +1,10 @@
 import 'package:flower_app/app/config/base_response/base_response.dart';
+import 'package:flower_app/app/feature/check_out/data/models/cash_on_delivery_dto.dart';
+import 'package:flower_app/app/feature/check_out/data/models/credit_card_dto.dart';
 import 'package:flower_app/app/feature/check_out/data/models/user_address_response.dart';
 
 abstract class CheckOutRemoteDataSourceContract {
   Future<BaseResponse<UserAddressResponse>> getUserAddressesResponse();
+  Future<BaseResponse<CashOnDeliveryDto>> checkOutWithCashOnDeliveryMethod({String? street,String? phone,String? city,String? lat,String? long});
+  Future<BaseResponse<CreditCardDto>> checkOutWithCreditCardMethod({String? street,String? phone,String? city,String? lat,String? long});
 }
