14a72d9415506ccc4f23564260fc35ff987cf405
momenhaitham <momenhaithamcod12@gmail.com>
2026-02-09 21:56:56 +0200

    fix: fixed a bug in address details screen related to the map and street textField

--- lib/app/feature/address_details/presentation/views/screens/address_details_screen.dart
+++ lib/app/feature/address_details/presentation/views/screens/address_details_screen.dart
@@ -42,11 +42,12 @@ class _AddressDetailsScreenState extends State<AddressDetailsScreen> {
 
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
-    if (widget.userAddressEntity != null) {
+    if (widget.userAddressEntity != null && widget.userAddressEntity?.lat != null && widget.userAddressEntity?.long != null) {
       viewmodel.doIntent(GetAddressFromCoordinatesEvent(
-        latitude: double.parse(widget.userAddressEntity!.lat!),
-        longitude: double.parse(widget.userAddressEntity!.long!)
+        latitude: double.tryParse(widget.userAddressEntity?.lat??"22")??0.0,
+        longitude: double.tryParse(widget.userAddressEntity?.long??"22")??0.0
       ));
+      viewmodel.area=widget.userAddressEntity?.city;
     }
     
     return Scaffold(
@@ -108,7 +109,7 @@ class _AddressDetailsScreenState extends State<AddressDetailsScreen> {
                         decoration: InputDecoration(
                           labelText: AppLocale(context).address,
                         ),
-                        initialValue: widget.userAddressEntity==null?null: viewmodel.address,
+                        initialValue: widget.userAddressEntity==null?viewmodel.address: null,
                         controller: viewmodel.addressController,
                         validator: (value) {
                           if (value == null || value.isEmpty) {
